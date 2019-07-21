439f5cf4479483c9f468a97b73d6f281b68b4c8d
Elves <c2431086250@163.com>
2019-07-21 11:25:50 +0800

    测试

--- src/test/java/com/ctao/O2O/service/ProductServiceTest.java
+++ src/test/java/com/ctao/O2O/service/ProductServiceTest.java
@@ -2,8 +2,10 @@ package com.ctao.O2O.service;
 
 import com.ctao.O2O.BaseTest;
 
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ProductServiceTest extends BaseTest {
     @Autowired
     private ProductService productService;
